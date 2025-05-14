b4c61f2d303678b0200387f4dc7e505a942597ff
Mahan Mehrabi <gnusmhn@gmail.com>
2025-05-14 14:48:52 +0330

    Add files via upload

---
+++ 002.Variables/main.py
@@ -0,0 +1,46 @@
+#Variable = A container for a value (string , integer , float , boolean)
+#           A variable behaves as if it was the value it contains
+
+#1.strings = text values (they can include numbers but can't do math operations with them)
+first_name = "Mahan" #first_name is the name of the variable
+print (first_name)
+
+#we can use our variables align with some text by simply using:
+print(f"Hello {first_name}")
+
+food = "pizza"
+print(f"I like {food}")
+
+email = "mahan123@fake.com"
+print (f"My E-mail is {email}")
+
+#2.integers = whole number that can be changed by math equations
+
+age = 17 #no "" or ''
+print (f"I am {age} years old")
+
+quantity = 3
+print (f"You are buying {quantity} items")
+
+numOfStudents = 30
+print (f"Your class has {numOfStudents} students")
+
+#3.float = numbers with decimal points
+price = 10.99
+print(f"The price is ${price}")
+
+gpa = 3.2
+print(f"Your gpa is {gpa}")
+
+distance = 5.5
+print (f"You ran {distance}km")
+
+#4.boolean = true or false
+isStudent = True
+print (f"Are you a student? {isStudent}")
+
+for_sale = False
+print (f"Is it for sal? {for_sale}")
+
+is_online = True
+print(f"Are you online? {is_online}")
