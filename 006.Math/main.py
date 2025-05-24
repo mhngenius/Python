4385d6148318bfb7762accbcabdad308f6beec6b
Mahan Mehrabi <gnusmhn@gmail.com>
2025-05-24 15:28:15 +0330

    Add files via upload

---
+++ 006.Math/main.py
@@ -0,0 +1,39 @@
+# Arithmetic & math
+
+friends = 5
+
+friends += 1 # augmented assignment operator # Addition
+friends = friends - 1 # Subtraction
+friends *= 3 # Multiplication
+friends = friends / 3 # Division
+
+# Exponentiation
+
+# "**" = to the power of
+friends = friends ** 2
+# friends **= 2
+
+# Modules = giving you the remainder of any division (the sign = %)
+
+remainder = friends % 3
+
+print(friends)
+print(remainder)
+
+# Built-In math functions
+
+x = 3.14
+y  = -4
+z = 5
+
+result = round(x) # round a number to the nearest whole number (integer)
+result2 = abs(y) # distance from 0 as a whole number (or basically the positive version (in this example))
+result3 = pow(z,3) # built-in power function = pow(base , exponent)
+result4 = max(x,y,z) # finds the maximum value among some variables or other objects
+result5 = min(x,y,z) # finds the minimum value among some variables or other objects
+
+print(result)
+print(result2)
+print(result3)
+print(result4)
+print(result5)
