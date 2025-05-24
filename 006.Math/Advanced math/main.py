f8cecc4ce6354949952f4d79fbaeb36810c69b29
Mahan Mehrabi <gnusmhn@gmail.com>
2025-05-24 15:37:04 +0330

    Add files via upload

---
+++ 006.Math/Advanced math/main.py
@@ -0,0 +1,9 @@
+import  math # first we have to import the "math" module
+
+print(math.pi) # value of "PI"
+print(math.e) # value of "e"
+print(math.sqrt(9)) # the square root of a value or a variable
+
+x = 9.5
+print(math.ceil(x)) # rounds a number upwards
+print(math.floor(x)) # rounds a number downwards
