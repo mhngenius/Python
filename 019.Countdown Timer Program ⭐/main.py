04912a3abe5cd36d47a10929e0496ab393a42d6d
Mahan Mehrabi <gnusmhn@gmail.com>
2025-08-25 13:53:41 +0330

    Add files via upload

---
+++ 019.Countdown Timer Program ⭐/main.py
@@ -0,0 +1,14 @@
+# countdown timer program
+
+import time
+
+my_time = int(input("Enter the time in seconds: "))
+
+for x in range (my_time,0,-1):
+    seconds = x % 60
+    minutes = int(x / 60) % 60
+    hours = int(x / 3600)
+    print(f"{hours:02}:{minutes:02}:{seconds:02}")
+    time.sleep(1)  # sleep function makes the program run after a specific amount of time (seconds)
+
+print("TIME IS UP !!!")
